32b8206aaa2f3b1d305c96627a2149b28c696a60
Ariful Islam <arif98741@gmail.com>
2023-03-15 01:25:58 +0600

    init

---
+++ src/LaravelBDSmsServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ *  Last Modified: 03/15/23, 11:18 PM
+ *  Copyright (c) 2023
+ *  -created by Ariful Islam
+ *  -All Rights Preserved By
+ *  -If you have any query then knock me at
+ *  arif98741@gmail.com
+ *  See my profile @ https://github.com/arif98741
+ */
+
+namespace Xenon\ActivityLog;
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
+
+class LaravelBDSmsServiceProvider extends ServiceProvider
+{
+
+    public function register()
+    {
+
+    }
+
+
+    public function boot(): void
+    {
+
+    }
+
+}
